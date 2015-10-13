c8fa1ce804d4e2b2c97c5de229bc72e8b249ed64
Manish Paranjape <manish@corra.com>
2015-10-13 12:26:05 -0400

    Revert "Update php.rb"
    This reverts commit 457ba82a5969409a13279bab918b942d7b58f8f5.

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -8,14 +8,20 @@ include_recipe "mod_php5_apache2"
 include_recipe "mod_php5_apache2::php"
 
 node[:deploy].each do |application, deploy|
-  
-  Chef::Log.debug("Running custom code")  
-  
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    next
+  end
+
   opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
     path deploy[:deploy_to]
   end
 
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
 end
 
