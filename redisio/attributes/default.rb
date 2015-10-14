1c45e79f3e24f916eb6b36339e471a7053426249
Manish Paranjape <manishparanjape@gmail.com>
2015-10-14 12:44:38 -0400

    Update default.rb

--- redisio/attributes/default.rb
+++ redisio/attributes/default.rb
@@ -89,5 +89,5 @@ default['redisio']['default_settings'] = {
 }
 
 # The default for this is set inside of the "install" recipe. This is due to the way deep merge handles arrays
-default['redisio']['servers'] = nil
+default['redisio']['servers'] = "#{node[:redis][:servers]}"
 
