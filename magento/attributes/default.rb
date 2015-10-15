a88a877357129cc2a8fa81fb54a1b43b7bb98fd1
Manish Paranjape <manishparanjape@gmail.com>
2015-10-15 11:13:19 -0400

    Update default.rb

--- magento/attributes/default.rb
+++ magento/attributes/default.rb
@@ -1,2 +1,3 @@
-normal[:mod_php5_apache2][:packages] = [ 'php5-curl', 'php5-gd', 'php5-cli', 'php5-mysql', 'php5-mcrypt' ]
+#REMOVED FOR NOW. NED TO REVISIT IF THE CURL ISSUE IS AMAZON LINUX SPECIFIC
+#normal[:mod_php5_apache2][:packages] = [ 'php5-curl', 'php5-gd', 'php5-cli', 'php5-mysql', 'php5-mcrypt' ]
 normal[:opsworks][:deploy_keep_releases] = 3
